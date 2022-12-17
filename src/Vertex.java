ecf1f3086c8b7fa19a644810dea429f3c9821a39
dnbabkov <dnbabkov@gmail.com>
2022-12-17 22:40:29 +0300

    Added something that is not working correctly

--- src/Vertex.java
+++ src/Vertex.java
@@ -12,26 +12,19 @@ public class Vertex {
         this.z = z;
     }
 
-    Point2D.Double screenTransition() {
-
-        double distance = 200;
+    Point2D.Double screenTransition(double distance) {
 
         return new Point2D.Double(distance * this.x / this.z, distance * this.y / this.z);
     }
 
     Vertex viewCoordinateTrans(Vertex camera) {
 
-        double r = Math.sqrt(Math.pow(camera.x, 2) + Math.pow(camera.y, 2) + Math.pow(camera.z, 2));
-        double zeta = Math.acos(camera.z/r);
-        double rxy = Math.sqrt(Math.pow(camera.x, 2) + Math.pow(camera.y, 2));
-        double phi = 0;
-        if (rxy != 0) {
-            phi = Math.acos(camera.x/rxy);
-        }
+        VertexSpherical s = new VertexSpherical(0, 0, 0);
+        s = s.convert(camera);
 
-        double xE = -this.x * Math.sin(phi) + this.y * Math.cos(phi);
-        double yE = -this.x * Math.cos(zeta) * Math.cos(phi) - this.y * Math.cos(zeta) * Math.sin(phi) + this.z * Math.sin(zeta);
-        double zE = -this.x * Math.sin(zeta) * Math.cos(phi) - this.y * Math.sin(zeta) * Math.sin(phi) - this.z * Math.cos(zeta) + r;
+        double xE = -this.x * Math.sin(s.phi) + this.y * Math.cos(s.phi);
+        double yE = -this.x * Math.cos(s.zeta) * Math.cos(s.phi) - this.y * Math.cos(s.zeta) * Math.sin(s.phi) + this.z * Math.sin(s.zeta);
+        double zE = -this.x * Math.sin(s.zeta) * Math.cos(s.phi) - this.y * Math.sin(s.zeta) * Math.sin(s.phi) - this.z * Math.cos(s.zeta) + s.r;
 
         return new Vertex(xE, yE, zE);
     }
--- src/Viewer.java
+++ src/Viewer.java
@@ -1,13 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.util.List;
 import java.util.ArrayList;
-import java.awt.image.BufferedImage;
 import java.awt.geom.*;
 
 public class Viewer {
-
-
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();
@@ -23,7 +21,9 @@ public class Viewer {
         JSlider pitchSlider = new JSlider(SwingConstants.VERTICAL, -180, 180, 0);
         pane.add(pitchSlider, BorderLayout.EAST);
 
+        final Vertex[] camera = {new Vertex(0, 0, -500)};
 
+        final double[] distance = {200};
 
         // panel to display render results
         JPanel renderPanel = new JPanel() {
@@ -32,27 +32,31 @@ public class Viewer {
                 g2.setColor(Color.BLACK);
                 g2.fillRect(0, 0, getWidth(), getHeight());
 
+                // 1 - (100, 100, 100)
+                // 2 - (-100, 100, 100)
+                // 3 - (-100, -100, 100)
+                // 4 - (100, -100, 100)
+                // 5 - (100, 100, -100)
+                // 6 - (-100, 100, -100)
+                // 7 - (-100, -100, -100)
+                // 8 - (100, -100, -100)
+                // ^
+                // |
+                // Грани - 1234, 2673, 6587, 5148, 5621, 4378
+
                 List<Square> cube = new ArrayList<>();
-                cube.add(new Square(new Vertex(100, 100, 100), new Vertex(-100, 100, 100), new Vertex(-100, -100, 100), new Vertex(100, -100, 100)));
-                cube.add(new Square(new Vertex(100, 100, 100), new Vertex(-100, 100, 100), new Vertex(-100, 100, -100), new Vertex(100, 100, -100)));
-                cube.add(new Square(new Vertex(100, 100, -100), new Vertex(-100, 100, -100), new Vertex(-100, -100, -100), new Vertex(100, -100, -100)));
-                cube.add(new Square(new Vertex(100, -100, -100), new Vertex(-100, -100, -100), new Vertex(-100, -100, 100), new Vertex(100, -100, 100)));
-                cube.add(new Square(new Vertex(100, 100, 100), new Vertex(100, 100, -100), new Vertex(100, -100, -100), new Vertex(100, -100, 100)));
-                cube.add(new Square(new Vertex(-100, 100, 100), new Vertex(-100, 100, -100), new Vertex(-100, -100, -100), new Vertex(-100, -100, 100)));
 
-                Vertex camera = new Vertex(130, 150, 200);
-
-                double pitch = -Math.toRadians(headingSlider.getValue());
-                double heading = -Math.toRadians(pitchSlider.getValue());
-                Matrix3 headingTransform = new Matrix3(new double[] {
-                        Math.cos(heading), 0, -Math.sin(heading),
-                        0, 1, 0,
-                        Math.sin(heading), 0, Math.cos(heading)
-                });
+                double heading = -Math.toRadians(headingSlider.getValue());
+                double pitch = -Math.toRadians(pitchSlider.getValue());
                 Matrix3 pitchTransform = new Matrix3(new double[] {
+                        Math.cos(pitch), 0, -Math.sin(pitch),
+                        0, 1, 0,
+                        Math.sin(pitch), 0, Math.cos(pitch)
+                });
+                Matrix3 headingTransform = new Matrix3(new double[] {
                         1, 0, 0,
-                        0, Math.cos(pitch), Math.sin(pitch),
-                        0, -Math.sin(pitch), Math.cos(pitch)
+                        0, Math.cos(heading), Math.sin(heading),
+                        0, -Math.sin(heading), Math.cos(heading)
                 });
 
                 g2.translate(getWidth()/2, getHeight()/2);
@@ -66,30 +70,151 @@ public class Viewer {
                     Vertex v3 = transform.transform(s.v3);
                     Vertex v4 = transform.transform(s.v4);
 
-                    v1 = v1.viewCoordinateTrans(camera);
-                    v2 = v2.viewCoordinateTrans(camera);
-                    v3 = v3.viewCoordinateTrans(camera);
-                    v4 = v4.viewCoordinateTrans(camera);
+                    Square sTemp = new Square(v1, v2, v3, v4);
 
-                    Point2D vs1 = v1.screenTransition();
-                    Point2D vs2 = v2.screenTransition();
-                    Point2D vs3 = v3.screenTransition();
-                    Point2D vs4 = v4.screenTransition();
+                    v1 = v1.viewCoordinateTrans(camera[0]);
+                    v2 = v2.viewCoordinateTrans(camera[0]);
+                    v3 = v3.viewCoordinateTrans(camera[0]);
+                    v4 = v4.viewCoordinateTrans(camera[0]);
 
-                    Path2D path = new Path2D.Double();
-                    path.moveTo(vs1.getX(), vs1.getY());
-                    path.lineTo(vs2.getX(), vs2.getY());
-                    path.lineTo(vs3.getX(), vs3.getY());
-                    path.lineTo(vs4.getX(), vs4.getY());
-                    path.closePath();
+                    Point2D vs1 = v1.screenTransition(distance[0]);
+                    Point2D vs2 = v2.screenTransition(distance[0]);
+                    Point2D vs3 = v3.screenTransition(distance[0]);
+                    Point2D vs4 = v4.screenTransition(distance[0]);
 
-                    g2.draw(path);
+                    if (sTemp.isVisible(camera[0])){
+
+                        Path2D path = new Path2D.Double();
+                        path.moveTo(vs1.getX(), vs1.getY());
+                        path.lineTo(vs2.getX(), vs2.getY());
+                        path.lineTo(vs3.getX(), vs3.getY());
+                        path.lineTo(vs4.getX(), vs4.getY());
+                        path.closePath();
+
+                        g2.draw(path);
+
+                    }
 
                 }
             }
         };
 
+        renderPanel.setFocusable(true);
+
         pane.add(renderPanel, BorderLayout.CENTER);
+
+        renderPanel.addMouseWheelListener(new MouseWheelListener() {
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                double multiplier = 1;
+                if(e.getWheelRotation()<0)
+                    for(int i = 0; i<-e.getWheelRotation();i++)
+                        multiplier*=1.1;
+                else
+                    for(int i = 0; i<e.getWheelRotation();i++)
+                        multiplier/=1.1;
+
+                distance[0] *= multiplier;
+                renderPanel.repaint();
+            }
+
+        });
+
+        final int[] previousX = {-1000};
+        final int[] currentX = {0};
+        final int[] previousY = {-1000};
+        final int[] currentY = {0};
+
+        renderPanel.addMouseMotionListener(new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                currentX[0] = e.getX();
+                if (previousX[0] == -1000){
+                    previousX[0] = currentX[0];
+                }
+                int deltaX = currentX[0] - previousX[0];
+                previousX[0] = currentX[0];
+
+                currentY[0] = e.getY();
+                if (previousY[0] == -1000){
+                    previousY[0] = currentY[0];
+                }
+                int deltaY = currentY[0] - previousY[0];
+                previousY[0] = currentY[0];
+
+                VertexSpherical spherical = new VertexSpherical(0, 0, 0);
+                spherical = spherical.convert(camera[0]);
+
+                spherical.phi -= Math.toRadians(deltaX);
+                spherical.zeta += Math.toRadians(deltaY);
+
+                if (spherical.phi >= Math.PI) {
+                    spherical.phi -= Math.PI;
+                }
+
+                if (spherical.phi <= 0) {
+                    spherical.phi += Math.PI;
+                }
+
+                if (spherical.zeta >= Math.PI) {
+                    spherical.zeta -= Math.PI;
+
+                }
+
+                if (spherical.zeta <= 0) {
+                    spherical.zeta += Math.PI;
+                }
+
+                camera[0].x = spherical.r * Math.sin(spherical.zeta) * Math.cos(spherical.phi);
+                camera[0].y = spherical.r * Math.sin(spherical.zeta) * Math.sin(spherical.phi);
+                camera[0].z = spherical.r * Math.cos(spherical.zeta);
+
+                renderPanel.repaint();
+
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+
+                previousX[0] = -1000;
+                previousY[0] = -1000;
+
+            }
+        });
+
+        renderPanel.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                int degOnX = 0;
+                int degOnZ = 0;
+
+                if(e.getKeyCode() == 68) {
+                    degOnX += 2;
+                }
+                if(e.getKeyCode() == 65) {
+                    degOnX += -2;
+                }
+                if(e.getKeyCode() == 87) {
+                    degOnZ += 2;
+                }
+                if(e.getKeyCode() == 83) {
+                    degOnZ += -2;
+                }
+
+
+
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
 
         headingSlider.addChangeListener(e -> renderPanel.repaint());
         pitchSlider.addChangeListener(e -> renderPanel.repaint());
